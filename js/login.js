e41d5a929e22b939115ebd2c2a6786aaf478289a
chentzriker <chentzriker@hilma.tech>
2023-10-15 18:23:54 +0300

    login page

--- js/login.js
+++ js/login.js
@@ -2,8 +2,14 @@ const log_in = document.getElementById("enter");
 log_in.addEventListener("click", checkUser);
 
 function checkUser () {
+    console.log("entered")
     const name = document.getElementById("Username").value;
     const password = document.getElementById("Password").value;
+    console.log(name)
+    if (!name || !password) {
+        alert ("please fill all feilds")
+        return;
+    }
     if (!localStorage.getItem(name)) {
         alert("משתמש לא קיים");
         return;
