113321bc09edb9b4c2dd1539fb1cb0a727d1dd60
chentzriker <chentzriker@hilma.tech>
2023-10-16 14:54:04 +0300

    organization by columns

--- js/4inarow.js
+++ js/4inarow.js
@@ -3,25 +3,30 @@ let board = [];
 function createDivs(rows, columns) {
     for (let i = 0; i < columns; i++) {
         console.log("entered")
-        for (let j = 0; j<rows; j++) {
+        let column = [];
+        for (let j = 0; j < rows; j++) {
             console.log("hi")
-            let row = [];
             let newElement = document.createElement("div")
-            newElement.classList.add("w")
+            newElement.classList.add("blank")
+            newElement.classList.add("cell")
             newElement.classList.add(i)
             document.getElementById("container").appendChild(newElement)
             newElement.addEventListener("click", turn)
-            row.push(newElement)
-            if (j==(rows-1)){
-                board.push(row);
-            }
-        
+            column.push(newElement)
         }
+        board.push(column);
     }
+    let container = document.getElementById("container")
+    container.style.backgroundColor = "black";
+    container.style.gridTemplateColumns = `repeat(${columns},auto)`
+    container.style.gridTemplateRows = `repeat(${rows},auto)`
+    console.log('container.style: ', container.style);
+
 }
-createDivs(6,7);
+createDivs(6, 7);
+console.log(board)
 
 //not finished, אמור לסמן את הריבוע בצבע
-function turn () {
+function turn() {
 
 }
