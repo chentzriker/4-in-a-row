0bd732376ab1ca61d908fd907b97dbdb7a81d781
chentzriker <chentzriker@hilma.tech>
2023-10-16 10:28:40 +0300

    fixed login page

--- js/login.js
+++ js/login.js
@@ -1,34 +1,22 @@
+// let arr = [{name:"chen", password:123}, {name:"efrat", password:1234}];
+// let item = JSON.stringify(arr)
+// localStorage.setItem("users",item)
 const log_in = document.getElementById("enter");
 log_in.addEventListener("click", checkUser);
+
 
 function checkUser () {
     console.log("entered")
     const name = document.getElementById("Username").value;
     const password = document.getElementById("Password").value;
     console.log(name)
-    if (!name || !password) {
-        alert ("please fill all feilds")
-        return;
+    let usersstr = localStorage.getItem("users");
+    let usersarr = JSON.parse(usersstr);
+    for (element of usersarr) {
+        if (element.name === name && element.password == password){
+             window.location.href = "../html/gamemenu.html"
+             return;
+        }
     }
-    if (!localStorage.getItem(name)) {
-        alert("משתמש לא קיים");
-        return;
-    }
-    let userDetails = localStorage.getItem(name);
-    let check = JSON.parse(userDetails);
-    if (check.password !== password ) {
-        alert("password is wrong")
-        return;
-    }
-    if (check.name !== name) {
-        alert("username is wrong")
-    }
-    window.location.href = "../html/gamemenu.html"
-    return;
-};
-//  const moving = document.getElementById("move");
-//  moving.addEventListener("click",movePage);
-
-//  function movePage (){
-//     window.location.href = "../html/gamemenu.html"
-//  } 
+    alert ("one field or more is wrong")
+}
