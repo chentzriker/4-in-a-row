c8a32a67e437743e37baa70ad302be339654bad7
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-10-16 17:27:48 +0300

    game menu js, html, css

---
+++ js/gamemenu.js
@@ -0,0 +1,8 @@
+const unavailableGames= document.getElementsByClassName("unavailable")
+for (let i = 0; i < unavailableGames.length; i++) {
+    unavailableGames[i].addEventListener("mouseover", unavailablePic() )
+}
+
+function unavailablePic(){
+    //unavailableGames[i].src
+}
